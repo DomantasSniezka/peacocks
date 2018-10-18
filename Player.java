dffdd93c56561289f9861dac4c84cca5e8146e40
DomantasSniezka <domantas.sniezka@ucdconnect.ie>
2018-10-18 15:57:25 +0100

    Software Engineering Project: Cluedo
    Languages used: Java

---
+++ Player.java
@@ -0,0 +1,86 @@
+package gameengine;
+
+import bots.BotAPI;
+
+public class Player {
+
+    private final String name;
+    private final Token token;
+    private Cards cards = new Cards();
+    private final Cards viewedCards = new Cards();
+    private boolean eliminated = false;
+    private boolean hasRolled = false;
+    private BotAPI bot;
+
+    Player(String name, Token token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean hasName(String name) {
+        return this.name.toLowerCase().trim().equals(name.toLowerCase().trim());
+    }
+
+    public Token getToken() {
+        return token;
+    }
+
+    void addCards(Cards cards) {
+        this.cards = cards;
+    }
+
+    public Cards getCards() {
+        return cards;
+    }
+
+    void addViewedCard(Card card) {
+        viewedCards.add(card);
+    }
+
+    public boolean hasSeen(String card) {
+        for (Card viewedCard : viewedCards) {
+            if (viewedCard.hasName(card)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void eliminate() {
+        eliminated = true;
+    }
+
+    public boolean isEliminated() {
+        return eliminated;
+    }
+
+    public boolean hasRolled() {return hasRolled;}
+
+    public void rolledDice() { hasRolled = true;}
+
+
+    public void endPlayerTurn()    {hasRolled = true;}
+
+    public void startPlayerTurn()    {hasRolled = false;}
+
+
+    public boolean hasCard(String name) {
+        return cards.contains (name);
+    }
+
+    void addBot (BotAPI bot) {
+        this.bot = bot;
+    }
+
+    BotAPI getBot () {
+        return bot;
+    }
+
+    public String toString() {
+        return name + " (" + token.getName() + ")";
+    }
+}
